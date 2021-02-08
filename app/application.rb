ac8fcdd009da4dc6fba9d5bdb409996d7d4091e3
DSJR1284 <DSJR1284@GMAIL.COM>
2021-02-08 14:16:07 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,20 +1,24 @@
 class Application 
 
-  @@items = []
-
+  @@items = [Item.new("Fender Bass", 879.99) , Item.new("Squire Bass", 599.99)]
+ 
   def call(env)
-    resp = Rack::Response.new 
-    req = Rack::Response.new(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.name == item_name}
-
-      resp.write item.name
-    else 
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else        
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else      
       resp.write "Route not found"
-      resp.status = 400
-    end 
-    resp.finish 
-  end 
+      resp.status=404
+    end
+    resp.finish
+  end
+  
 end 

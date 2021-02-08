15f8f539ceb85137003bcbc136be0f356d2bb934
DSJR1284 <DSJR1284@GMAIL.COM>
2021-02-08 13:52:22 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application 
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Response.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+
+      resp.write item.name
+    else 
+      resp.write "Route not found"
+      resp.status = 400
+    end 
+    resp.finish 
+  end 
+end 
